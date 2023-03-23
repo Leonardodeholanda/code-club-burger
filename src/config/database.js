94a8f3622aacd6ff4680131bd53b36fae9aad642
leonardodeholanda <leonardohvrocha@gmail.com>
2023-03-23 12:34:19 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'postgress',
+    database: 'codeburger',
+    define: {
+        timestamps: true,
+        urderScored: true,
+        underScoredAll: true,
+    }
+}
