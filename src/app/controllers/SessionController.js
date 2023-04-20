5c6729a82579ecb0050e8d34f81da0cc3089c835
leonardodeholanda <leonardohvrocha@gmail.com>
2023-04-20 10:12:32 -0300

    second commit

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,51 @@
+import * as Yup from 'yup'
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth'
+import User from '../models/User'
+
+class SessionController{
+    async store(request, response) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+            password: Yup.string().required(),
+        })
+
+        const userMailOrPasswordIncorrect = () => {
+            return response.status(401).json({ error: "Make sure your password or email are correct" })
+        }
+
+        if(!(await schema.isValid(request.body))) userMailOrPasswordIncorrect()
+
+        const { email, password } = request.body
+
+        const user = await User.findOne({
+            where: { email },
+        })
+
+       /* if (!user) {
+            return userMailOrPasswordIncorrect()
+        }
+        
+        const passwordMatches = await user.checkPassword(password)
+        
+        if (!passwordMatches) {
+            return userMailOrPasswordIncorrect()
+        }
+*/
+        if(!user) userMailOrPasswordIncorrect()
+
+        if (!(await user.checkPassword(password))) userMailOrPasswordIncorrect()
+
+        return response.json({
+             id: user.id, 
+             email, 
+             name: user.name, 
+             admin: user.admin,
+             token: jwt.sign({ id: user.id, name: user.name }, authConfig.secret, {
+                expiresIn: authConfig.expiresIn,
+             } )
+        })
+    }
+}
+
+export default new SessionController()
--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,35 @@
 import Sequelize from 'sequelize'
+import mongoose from 'mongoose'
 
+import Product from '../app/models/Product'
 import User from '../app/models/User'
+import Category from '../app/models/Category'
 
 import configDataBase from '../config/database'
 
-const models = [ User ]
+const models = [ User, Product, Category ]
 
 class Database {
     constructor(){
         this.init()
+        this.mongo()
     }
 
     init(){
         this.connection = new Sequelize(configDataBase)
-        models.map((model) => model.init(this.connection))
+        models.map((model) => model.init(this.connection)).map(
+            (model) => model.associate && model.associate(this.connection.models)
+        )
     }
+    mongo(){
+        mongoose.set('strictQuery', false)
+        this.mongoConection = mongoose.connect('mongodb://localhost:27017/codeburger',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+    }
+    
 }
 
 export default new Database()
